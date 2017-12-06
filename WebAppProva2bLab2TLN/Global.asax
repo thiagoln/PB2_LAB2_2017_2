827eaf01a24ee59ad805d8dd4e477a74310664a8
Thiago Lemes <thigo_bl@hotmail.com>
2017-12-06 19:39:42 -0300

    Prova

---
+++ WebAppProva2bLab2TLN/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppProva2bLab2TLN.MvcApplication" Language="C#" %>
---
+++ WebAppProva2bLab2TLN/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppProva2bLab2TLN.Startup))]
+namespace WebAppProva2bLab2TLN
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
